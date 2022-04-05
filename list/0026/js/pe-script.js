840270c6920fc5e6c451a6b35b13cd5faaac1622
yife68 <2714344056@qq.com>
2022-04-05 13:04:07 +0800

    12、13集更新

--- list/0026/js/pe-script.js
+++ list/0026/js/pe-script.js
@@ -90,6 +90,18 @@ let All_song = [
    path: "https://res.wx.qq.com/voice/getvoice?mediaid=MzkyOTI3MTA4MV8xMDAwMDA1NDA=",
    img: "https://yife68.gitee.io/icat-pic-2022/03/03/3.png",
    singer: "第十一集"
+ },
+ {
+   name: "重生之将门毒后",
+   path: "https://res.wx.qq.com/voice/getvoice?mediaid=MzkyOTI3MTA4MV8xMDAwMDA2MTM=",
+   img: "https://yife68.gitee.io/icat-pic-2022/03/03/3.png",
+   singer: "第十二集"
+ },
+ {
+   name: "重生之将门毒后",
+   path: "https://res.wx.qq.com/voice/getvoice?mediaid=MzkyOTI3MTA4MV8xMDAwMDA2MTQ=",
+   img: "https://yife68.gitee.io/icat-pic-2022/03/03/3.png",
+   singer: "第十三集"
  }
 ];
 
